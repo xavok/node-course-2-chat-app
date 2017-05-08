58d884edc104a9963d1d36bab64069b85e3682ba
Xavok <xavoking@gmail.com>
2017-05-08 06:47:40 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'user',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -44,14 +45,17 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled','disabled').text('Sending location...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Sending location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         });
 
     }, function () {
+        locationButton.removeAttr('disabled').text('Sending location');
         alert('Unable to fetch location');
     });
 });
