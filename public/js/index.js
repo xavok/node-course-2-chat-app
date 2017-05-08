77a50ea51623e3a2a1af12cf2934e73750547f8e
Xavok <xavoking@gmail.com>
2017-05-08 06:11:38 -0700

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,17 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function (message) {
     console.log('create new message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+       from: 'user',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
 });
